f0fa658abcac7d88a8b4fa0962c0233c424a9bd4
TejaswiChava <teja.chava4@gmail.com>
2019-03-13 18:12:59 +0530

    environment screen

--- src/app/components/environment/environment.component.ts
+++ src/app/components/environment/environment.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { CdtaService } from 'src/app/cdta.service';
+import { ElectronService } from 'ngx-electron';
+import { Router } from '@angular/router';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-environment',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./environment.component.css']
 })
 export class EnvironmentComponent implements OnInit {
+  showView = false;
+  isFromSetup = false;
+  environments = [{"name" : "DEMO","value":"demo"},{"name" : "DEV", "value": "dev"},{"name" : "INTG", "value": "intg"},{"name" : "LOCAL", "value": "local"},{"name" : "PREP", "value": "prep"},{"name" : "QA", "value": "qa"},{"name" : "QE", "value": "qe"},{"name" : "STAGING", "value": "staging"},{"name" : "UAT", "value": "uat"}]
 
-  constructor() { }
+  constructor(private cdtaservice: CdtaService, private electronService:ElectronService, private router: Router, private _ngZone: NgZone) {
+    console.log(environment.production)
+    this.electronService.ipcRenderer.on('switchLoginCallResult', (event, data) => {
+      if (data != undefined && data != "" && this.isFromSetup) {
+          this.isFromSetup = false;
+          localStorage.setItem('terminalConfigJson',data)
+          this._ngZone.run(() => {
+            if(JSON.parse(data).EquipmentId !=0){
+              this.cdtaservice.announceHeaderShowHide("hideHeader");
+              this.router.navigate(['/login'])
+            } else {
+                this.showView = true;
+            }
+          });
+      }
+  });
+  if(environment.production) {
+    this.router.navigate(['/setup'])
+  }
+  }
 
   ngOnInit() {
+    this.cdtaservice.announceHeaderShowHide("hideHeader");
+    this.isFromSetup = true;
+    this.electronService.ipcRenderer.send("switchlogincall");
   }
+
+  selectEnvironment(environment) {
+    localStorage.setItem('environment',environment.value)
+    this.router.navigate(['/setup'])
+  }
+
 
 }
