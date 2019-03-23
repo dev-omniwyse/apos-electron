d4ebbcf8f9a79bea5aaeb4f3274838b35881bc94
Dhariya <dariyahussain220>
2019-03-23 16:15:22 +0530

    remove product

--- src/app/services/Transaction.service.ts
+++ src/app/services/Transaction.service.ts
@@ -1,3 +1,21 @@
-//save transaction
 
-//
+export class TransactionService {
+    private static _transactionService = new TransactionService();
+
+
+    public static get getInstance() {
+        return TransactionService._transactionService;
+    }
+
+    constructor() {
+
+        if (TransactionService._transactionService) {
+            throw new Error("Error: Instantiation failed: Use ShoppingCartService.getInstance() instead of new.");
+        }
+        TransactionService._transactionService = this;
+    }
+
+    saveTransaction(shoppingCart){
+        
+    }
+}
