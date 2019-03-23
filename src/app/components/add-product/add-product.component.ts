74c94decee3a9fdee40b0eecafa6192c954cb404
mounu12 <kanugantimamatha@gmail.com>
2019-03-23 19:38:07 +0530

    product limit

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -11,6 +11,7 @@ import { Globals } from 'src/app/global';
 import { ShoppingCartService } from 'src/app/services/ShoppingCart.service';
 import { FilterOfferings } from 'src/app/services/FilterOfferings.service';
 import { MediaType, TICKET_GROUP, TICKET_TYPE } from 'src/app/services/MediaType';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 declare var pcsc: any;
 declare var $: any;
 var pcs = pcsc();
@@ -127,19 +128,21 @@ export class AddProductComponent implements OnInit {
   quantityList: any = [];
   viewCardData: any = []
   cardProductData: any = [];
-  
+
   productCheckOut: boolean = false;
   isNew: boolean = false;
-  slideConfig = {"slidesToShow": 2, dots:true,   "infinite": false,
-  "autoplay": false,   "prevArrow": false, "slidesToScroll": 2,
-  "nextArrow": false};
+  slideConfig = {
+    "slidesToShow": 2, dots: true, "infinite": false,
+    "autoplay": false, "prevArrow": false, "slidesToScroll": 2,
+    "nextArrow": false
+  };
 
 
 
-  currentWalletLineItem: any =[];
+  currentWalletLineItem: any = [];
 
   @ViewChildren('cardsList') cardsList;
-  constructor(private cdtaService?: CdtaService,private globals?: Globals, private route?: ActivatedRoute, private router?: Router, private _ngZone?: NgZone, private electronService?: ElectronService, ) {
+  constructor(private cdtaService?: CdtaService, private globals?: Globals, private route?: ActivatedRoute, private router?: Router, private _ngZone?: NgZone, private electronService?: ElectronService, ) {
     route.params.subscribe(val => {
       this.isMagnetic = localStorage.getItem("isMagnetic") == "true" ? true : false;
       this.isMerchendise = localStorage.getItem("isNonFareProduct") == "true" ? true : false;
@@ -158,16 +161,16 @@ export class AddProductComponent implements OnInit {
       }
       this.terminalConfigJson = JSON.parse(localStorage.getItem('terminalConfigJson'));
       console.log(this.readCarddata);
-      if(!this.isMagnetic) {
-      this.checkIsCardNew();
+      if (!this.isMagnetic) {
+        this.checkIsCardNew();
       }
       if (this.isMerchendise) {
         this.clickOnMerch();
       }
       else
         // this.frequentRide();
-    this.shoppingcart =JSON.parse(localStorage.getItem("shoppingCart"));
-    this.activeWallet(this.shoppingcart._walletLineItem[this.shoppingcart._walletLineItem.length-1]);
+        this.shoppingcart = JSON.parse(localStorage.getItem("shoppingCart"));
+      this.activeWallet(this.shoppingcart._walletLineItem[this.shoppingcart._walletLineItem.length - 1]);
 
 
       // if (this.isMagnetic) {
@@ -200,12 +203,12 @@ export class AddProductComponent implements OnInit {
         this._ngZone.run(() => {
           localStorage.setItem("readCardData", JSON.stringify(data));
           this.carddata = new Array(JSON.parse(data));
-         let item = JSON.parse(JSON.parse(localStorage.getItem("catalogJSON")));
-         let newshoppingCart= JSON.parse(localStorage.getItem('shoppingCart'));
-         let shoppingCart = FareCardService.getInstance.addSmartCard(newshoppingCart, this.carddata[0], item.Offering);
-         
-         ShoppingCartService.getInstance.shoppingCart = null;
-         console.log(shoppingCart);
+          let item = JSON.parse(JSON.parse(localStorage.getItem("catalogJSON")));
+          let newshoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+          let shoppingCart = FareCardService.getInstance.addSmartCard(newshoppingCart, this.carddata[0], item.Offering);
+
+          ShoppingCartService.getInstance.shoppingCart = null;
+          console.log(shoppingCart);
           this.cardJson.forEach(element => {
             if (element.printed_id == JSON.parse(data).printed_id) {
               isDuplicateCard = true;
@@ -221,7 +224,7 @@ export class AddProductComponent implements OnInit {
             this.selectCard(this.cardJson.length - 1);
             localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
             this.shoppingcart = JSON.parse(localStorage.getItem('shoppingCart'));
-            this.activeWallet(this.shoppingcart._walletLineItem[this.shoppingcart._walletLineItem.length-1])
+            this.activeWallet(this.shoppingcart._walletLineItem[this.shoppingcart._walletLineItem.length - 1])
           }
         });
       }
@@ -309,7 +312,7 @@ export class AddProductComponent implements OnInit {
     this.viewCardData = new Array(JSON.parse(item))
     this.cardProductData = JSON.parse(localStorage.getItem("cardProductData"))
     console.log("viewCardData", this.viewCardData)
-    this.shoppingcart =JSON.parse(localStorage.getItem("shoppingCart"));
+    this.shoppingcart = JSON.parse(localStorage.getItem("shoppingCart"));
     console.log(this.shoppingcart);
     this.frequentRide();
     // let item = JSON.parse(localStorage.getItem("catalogJSON"));
@@ -371,159 +374,166 @@ export class AddProductComponent implements OnInit {
     var existingQuantity = 0;
     this.currentCard.products.forEach(element => {
       if (selectedItem.Ticket.Group == element.product_type && (selectedItem.Ticket.Designator == element.designator)) {
-        if ((element.product_type == 1 && element.recharges_pending <= this.terminalConfigJson.MaxPendingCount) ||
-          (element.product_type == 2 && ((element.recharge_rides + selectedItem.Ticket.Value) <= 255)) ||
-          (element.product_type == 3 && ((element.remaining_value + (selectedItem.Ticket.Price * 100)) / 100) <= (this.terminalConfigJson.MaxStoredValueAmount / 100))) {
-          if (element.product_type == 1 && element.recharges_pending >= this.terminalConfigJson.MaxPendingCount) {
-            this.maxRechargesPendingReached = true;
-            this.maxLimitErrorMessages = "Max pending product limit reached.";
-            return;
-          }
-          else if (element.product_type == 2 && ((element.recharge_rides + selectedItem.Ticket.Value) >= 255)) {
-            this.maxRechargeRidesReached = true;
-            this.maxLimitErrorMessages = "Max stores ride limit reached.";
-            return;
-          }
-          else if (element.product_type == 3 && (((element.remaining_value + (selectedItem.Ticket.Price * 100)) / 100) >= (this.terminalConfigJson.MaxStoredValueAmount / 100))) {
-            this.maxRemainingValueReached = true;
-            this.maxLimitErrorMessages = "Max stored value limit reached.";
-            return;
-          }
-          // cardElement.recharges_pending
-          isExistingProducts = true;
-          if (element.product_type == 1) {
-            existingQuantity = element.recharges_pending;
-          }
-          // else if (element.product_type == 2) {
-          //   existingQuantity = (element.remaining_rides / selectedItem.Ticket.Value);
-          // }
-          this.areExistingProducts.push(true);
-        }
-        else {
-          isExistingProducts = false;
-        }
 
-      }
-    });
-    var quantityCount = 0;
-    var productCount = 0;
-    var isQuantityReached = false;
-    if (!isExistingProducts) {
-      this.areExistingProducts.push(false);
-      if ((this.currentCard.products.length == this.terminalConfigJson.NumberOfProducts)) {
-        isProductLimitReached = true
-      }
-      for (let index = 0; index < this.merchantList.length; index++) {
-        const product = this.merchantList[index];
-        if (selectedItem.Ticket.Group == 1 && this.currentCard.printed_id == this.productCardList[index] && selectedItem.OfferingId == this.merchantList[index].OfferingId) {
-          quantityCount = this.quantityList[index];
+        switch (element.product_type) {
+          case 1:
+            if (element.recharges_pending >= this.terminalConfigJson.MaxPendingCount)
+              isProductLimitReached = true;
+            break;
+          case 2:
+            if ((element.recharge_rides + selectedItem.Ticket.Value) >= 255)
+              isProductLimitReached = true;
+            break;
+          case 3:
+            if (element.product_type == 3 && (((element.remaining_value + (selectedItem.Ticket.Price * 100)) / 100) >= (this.terminalConfigJson.MaxStoredValueAmount / 100))) {
+              isProductLimitReached = true;
+              break;
         }
+        if (isProductLimitReached)
+          break;
+        return isProductLimitReached
       }
-      if (quantityCount >= this.terminalConfigJson.NumberOfProducts) {
-        isProductLimitReached = true;
-      }
-      this.productCardList.forEach(element => {
-        if (selectedItem.Ticket.Group == 1 && this.currentCard.printed_id == element) {
-          productCount++;
-        }
-      });
-      if (productCount >= this.terminalConfigJson.NumberOfProducts && quantityCount == 0) {
-        isProductLimitReached = true;
-      }
-
-
-
-    } else {
-      var existingProductCount = 0;
-      for (let index1 = 0; index1 < this.merchantList.length; index1++) {
-        const existingProduct = this.merchantList[index1];
-        if (this.currentCard.printed_id == this.productCardList[index1] && (existingProduct.OfferingId == selectedItem.OfferingId)) {
-          existingProductCount = this.quantityList[index1];
-        }
-      }
-      if (selectedItem.Ticket.Group == 1 && ((existingQuantity + existingProductCount + 1) > this.terminalConfigJson.MaxPendingCount)) {
-        isProductLimitReached = true
-      }
-
     }
-    // check if max products count reached
-    // for (let index1 = 0; index1 < this.merchantList.length; index1++) {
-    //   const element = this.merchantList[index1];
-    //   if (this.currentCard.printed_id == this.productCardList[index1] && element.OfferingId == this.smartCradProductToRemove.OfferingId) {
-    //     itemIndex = index1;
-    //   }
-    // }
-    // if (selectedItem.quantity == undefined) {
-    //   selectedItem.quantity = 0;
-    // }
-    // if (!isExistingProducts) {
-    //   this.areExistingProducts.push(false);
-    //   if ((this.currentCard.products.length == this.terminalConfigJson.NumberOfProducts) || (selectedItem.quantity == this.terminalConfigJson.NumberOfProducts)) {
-    //     isProductLimitReached = true
-    //   }
-    //   if (selectedItem.quantity == this.terminalConfigJson.NumberOfProducts) {
-    //     isProductLimitReached = true
-    //   }
-    // } else {
-    //   if (selectedItem.Ticket.Group == 1 && ((existingQuantity + (selectedItem.quantity + 1)) > this.terminalConfigJson.MaxPendingCount)) {
-    //     isProductLimitReached = true
-    //   }
-    //   else if (selectedItem.Ticket.Group == 2 && ((existingQuantity + (selectedItem.quantity + 1)) > 4)) {
-    //     isProductLimitReached = true;
-    //   }
-    // }
-    return isProductLimitReached;
+  })
   }
+
+
+    isTotalproductCountForCardreached(selectedItem: any) {
+      var canAddProduct = false
+      //console.log(this.currentWalletLineItem);
+      //for(let wc of this.currentWalletLineItem._walletContents) {
+      this.currentWalletLineItem._walletContents.forEach(walletContent => {
+        if(canAddProduct)
+          return;
+        if ( walletContent.offering.OfferingId == selectedItem.OfferingId ) {
+          canAddProduct = true;
+        }
+      })
+      
+      if( !canAddProduct && this.currentWalletLineItem._walletContents.length < 4) {
+          canAddProduct = true;
+          return canAddProduct;
+      }
+      return canAddProduct
+    // })
+  }
+
+  //     canAddProductToWallet(product:any){
+  //       var existingCount = this.getProductCountForExistingCard(product)
+  //       var shoppingCardCount = this.getProductCountFromShoppingCart(product: any);
+  //       isProductLimitReached = false;
+  //       switch (product.product_type) {
+  //         case 1:
+  //         if (existingCount + shoppingCardCount + product.value >= this.terminalConfigJson.MaxPendingCount)
+  //           isProductLimitReached = true;
+  //         break;
+  //       case 2:
+  //         if ((element.recharge_rides + selectedItem.Ticket.Value) >= 255)
+  //           isProductLimitReached = true;
+  //         break;
+  //       case 3:
+  //         if (existingCount + shoppingCardCount) + (selectedItem.Ticket.Price * 100)) / 100) >= (this.terminalConfigJson.MaxStoredValueAmount / 100))) {
+  //           isProductLimitReached = true;
+  //           break;
+  //     }
+  //     return isProductLimitReached
+  //   }
+  // }
+
+    getProductCountForExistingCard(selectedItem: any) {
+      this.currentCard.products.forEach(element => {
+        if (selectedItem.Ticket.Group == element.product_type && (selectedItem.Ticket.Designator == element.designator)) {
+          switch (element.product_type) {
+            case 1:
+              return element.recharges_pending
+              break;
+            case 2:
+              return element.recharge_rides
+              break;
+            case 3:
+              return element.remaining_value / 100
+              break;
+            default:
+              break;
+          }
+        }
+      })
+    }
+
+      // getProductCountFromShoppingCart(product: any) {
+      //   var isExistingProducts = false;
+      //   var isProductLimitReached = false;
+      //   var existingQuantity = 0;
+      //   this.currentWalletLineItem.forEach(walletContent => {
+      //     if ( walletContent.offeringId == selectedItem.offeringId )
+      //       switch (selectedItem.product_type) {
+      //         case 1:
+      //           return walletContent.Offering.qty;
+      //           break;
+      //         case 2:
+      //           return walletContent.remaining_value * walletContent.qty
+      //           break;
+      //         case 3:
+      //           return (walletContent.remaining_value * walletContent.qty)/100
+      //           break;
+      //       }
+      //     })
+      // }
+    
+     
+
 
   addProductToWallet(product) {
-    console.log(product);
-    console.log(this.shoppingcart);
+  if(!this.isTotalproductCountForCardreached(product)) {
+    this.maxLimitErrorMessages = "Max pending product limit reached.";
+    $("#maxCardLimitModal").modal('show');
+    return;
+  }
     let response = FareCardService.getInstance.addFareProduct(this.shoppingcart, product, this.currentWalletLineItem);
-    console.log(response);
-    console.log(this.currentWalletLineItem);
     localStorage.setItem('shoppingCart', JSON.stringify(response));
   }
-  getSelectedProductData(merch) {
-    if (this.isMaxProductLengthReached(merch)) {
-      if (this.maxRechargesPendingReached || this.maxRechargeRidesReached || this.maxRemainingValueReached) {
-        this.maxRechargesPendingReached = false;
-        this.maxRechargeRidesReached = false;
-        this.maxRemainingValueReached = false;
-        $("#maxCardLimitModal").modal('show');
-        return
-      }
-      $("#magneticCardLimitModal").modal('show');
-      return;
-    }
 
-    var isItemExistsInCard: any = false;
-    var itemIndex = 0;
-    for (let index = 0; index < this.productCardList.length; index++) {
-      const element = this.productCardList[index];
-      if (this.currentCard.printed_id == element && this.merchantList[index].OfferingId == merch.OfferingId) {
-        isItemExistsInCard = true;
-        itemIndex = index;
-      }
-    }
+  // getSelectedProductData(merch) {
+  //   if (this.isMaxProductLengthReached(merch)) {
+  //     if (this.maxRechargesPendingReached || this.maxRechargeRidesReached || this.maxRemainingValueReached) {
+  //       this.maxRechargesPendingReached = false;
+  //       this.maxRechargeRidesReached = false;
+  //       this.maxRemainingValueReached = false;
+  //       $("#maxCardLimitModal").modal('show');
+  //       return
+  //     }
+  //     $("#magneticCardLimitModal").modal('show');
+  //     return;
+  //   }
 
-    if (this.merchantList.length == 0) {
-      this.merchantList.push(merch);
-      this.productCardList.push(this.currentCard.printed_id)
-      this.quantityList.push(1);
-    }
-    else if (isItemExistsInCard == true) {
-      this.quantityList[itemIndex] = this.quantityList[itemIndex] + 1;
-    }
-    else if (isItemExistsInCard == false) {
-      this.quantityList.push(1);
-      this.merchantList.push(merch);
-      this.productCardList.push(this.currentCard.printed_id)
-    }
+  //   var isItemExistsInCard: any = false;
+  //   var itemIndex = 0;
+  //   for (let index = 0; index < this.productCardList.length; index++) {
+  //     const element = this.productCardList[index];
+  //     if (this.currentCard.printed_id == element && this.merchantList[index].OfferingId == merch.OfferingId) {
+  //       isItemExistsInCard = true;
+  //       itemIndex = index;
+  //     }
+  //   }
 
-    this.displaySmartCardsSubtotal(this.merchantList, false);
+  //   if (this.merchantList.length == 0) {
+  //     this.merchantList.push(merch);
+  //     this.productCardList.push(this.currentCard.printed_id)
+  //     this.quantityList.push(1);
+  //   }
+  //   else if (isItemExistsInCard == true) {
+  //     this.quantityList[itemIndex] = this.quantityList[itemIndex] + 1;
+  //   }
+  //   else if (isItemExistsInCard == false) {
+  //     this.quantityList.push(1);
+  //     this.merchantList.push(merch);
+  //     this.productCardList.push(this.currentCard.printed_id)
+  //   }
 
-    this.productTotal = this.productTotal + parseFloat(merch.Ticket.Price);
-  }
+  //   this.displaySmartCardsSubtotal(this.merchantList, false);
+
+  //   this.productTotal = this.productTotal + parseFloat(merch.Ticket.Price);
+  // }
 
   getSelectedMerchProductData(merch) {
     console.log(merch);
@@ -741,7 +751,7 @@ export class AddProductComponent implements OnInit {
     //     this.merchantise.push(element);
     //   }
     // });
-  
+
     console.log(list);
     this.merchantise = list;
   }
@@ -812,12 +822,12 @@ export class AddProductComponent implements OnInit {
     //new code
 
     let item = JSON.parse(JSON.parse(localStorage.getItem("catalogJSON")));
-    let newshoppingCart= JSON.parse(localStorage.getItem('shoppingCart'));
+    let newshoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
     let shoppingCart = FareCardService.getInstance.addMagneticsCard(newshoppingCart, item.Offering);
     ShoppingCartService.getInstance.shoppingCart = null;
     localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
     this.shoppingcart = JSON.parse(localStorage.getItem('shoppingCart'));
-    this.activeWallet(shoppingCart._walletLineItem[shoppingCart._walletLineItem.length-1]);
+    this.activeWallet(shoppingCart._walletLineItem[shoppingCart._walletLineItem.length - 1]);
   }
 
   // Boolean getWalletTypeID(walletList:any []) {
@@ -859,18 +869,18 @@ export class AddProductComponent implements OnInit {
   }
   activeWallet(item) {
     this.currentWalletLineItem = item;
-
-    if(this.shoppingcart._walletLineItem[0]._walletTypeId == item._walletTypeId) {
+    console.log(this.currentWalletLineItem);
+    if (this.shoppingcart._walletLineItem[0]._walletTypeId == item._walletTypeId) {
       this.isMerchendise = true;
       this.clickOnMerch();
-      
+
     }
     else {
       this.nonFare = true;
       this.regularRoute = false;
       this.isMerchendise = false;
       (this.selectedProductCategoryIndex == 0) ? this.frequentRide() : (this.selectedProductCategoryIndex == 1) ? this.storedValue() : this.payValue();
-   
+
     }
     //frequentRider
     //storedRide
